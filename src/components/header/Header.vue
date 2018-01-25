7ff41e52d3bc5f61a2f9b5679412d5eee8db6064
couturierdea@gmail.com <phassscode1>
2018-01-25 21:08:25 +0200

    Add RouterNav component

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -5,25 +5,18 @@
                 <img class="header__logo-image" src="~assets/img/logo.png" alt="Vue js">
                 <div class="header__logo-description">Vue app example</div>
             </div>
-            <nav class="header__nav">
-                <router-link class="header__nav-link"
-                             v-for="(route,key) in routesGetter"
-                             :key="key"
-                             :to="`${route.path}`"
-                             v-text="route.name"/>
-            </nav>
-
+            <RouterNav :class_name="'header'"></RouterNav>
         </div>
     </header>
 </template>
 
 <script>
+    import RouterNav from '../../router/RouterNav.vue'
+
     export default {
-        computed: {
-            routesGetter() {
-                return this.$router.options.routes;
-            }
-        }
+        components: {
+            RouterNav
+        },
     }
 </script>
 
---
+++ src/router/RouterNav.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav :class="`${class_name}__nav`">
+        <router-link :class="`${class_name}__nav-link`"
+                     v-for="(route,key) in routesGetter"
+                     :key="key"
+                     :to="`${route.path}`"
+                     v-text="route.name"/>
+    </nav>
+</template>
+
+<script>
+    export default {
+        props:{
+          class_name : {
+              default : ()=>{
+                  return 'header'
+              }
+          }
+        },
+        computed: {
+            routesGetter() {
+                return this.$router.options.routes;
+            }
+        }
+    }
+</script>
+
+<style src="./RouterNav.scss" lang="scss"></style>
